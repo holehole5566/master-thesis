1ee022820f19378737f55c7b511c0aab320a4d6a
holehole5566 <jd965303@gmail.com>
2023-11-20 23:08:14 +0800

    update

--- audioldm2.py
+++ audioldm2.py
@@ -1,12 +1,14 @@
 from diffusers import AudioLDM2Pipeline
 import torch
 import scipy
+import random
 repo_id = "cvssp/audioldm2-music"
 pipe = AudioLDM2Pipeline.from_pretrained(repo_id, torch_dtype = torch.float16)
 pipe = pipe.to("cuda")
-generator = torch.Generator("cuda").manual_seed(0)
+
 negative_prompt = "Low quality."
-prompt = "A funky bass guitar grooving in sync with drums."
-for i in range(3):
-  audio = pipe(prompt, num_inference_steps = 200, negative_prompt = negative_prompt, audio_length_in_s = 20.0, generator = generator).audios[0]
-  scipy.io.wavfile.write(str(i) + ".wav", rate = 16000, data = audio)
+prompt = "a composition with a sad mood, a slow rhythm, a low pitch, a soft intensity, and a mellow timbre."
+for i in range(30):
+  generator = torch.Generator("cuda").manual_seed(random.randint(0, 100000))
+  audio = pipe(prompt, num_inference_steps = 250, negative_prompt = negative_prompt, audio_length_in_s = 15.0, generator = generator).audios[0]
+  scipy.io.wavfile.write("generated/{}.mp3".format(str(i)), rate = 16000, data = audio)
---
+++ classification.py
@@ -0,0 +1,55 @@
+import torch.nn.functional as F
+import json
+from msclap import CLAP
+from collections import defaultdict 
+
+feature_classes = [['fast rhythm','slow rhythm'],['high pitch','low pitch'],['loud intensity','soft intensity'],['bright timbre','mellow timbre']]
+eval_classes = [['high arousal','low arousal'],['positive valence','negative valence']]
+
+result_dict = {}
+prompt = 'music with '
+
+n = 251
+
+for classes in feature_classes:
+    
+    class_prompts = []
+    
+    for desc in classes:
+        class_prompts.append(prompt + desc)
+    
+    print(class_prompts)
+    
+    result = defaultdict(float)
+    
+    for i in range(n):
+
+        audio_files = []
+        music = 'music/music (' + str(i+1) + ').mp3'
+        audio_files.append(music)
+        # Load and initialize CLAP
+        # Setting use_cuda = True will load the model on a GPU using CUDA
+        clap_model = CLAP(version = '2023', use_cuda=True)
+
+        # compute text embeddings from natural text
+        text_embeddings = clap_model.get_text_embeddings(class_prompts)
+
+        # compute the audio embeddings from an audio file
+        audio_embeddings = clap_model.get_audio_embeddings(audio_files, resample=True)
+
+        # compute the similarity between audio_embeddings and text_embeddings
+        similarity = clap_model.compute_similarity(audio_embeddings, text_embeddings)
+
+        similarity = F.softmax(similarity, dim=1)
+        values, indices = similarity[0].topk(2)
+
+        for value, index in zip(values, indices):
+             #print(f"{classes[index]:>16s}: {100 * value.item():.2f}%")
+            result[classes[index]] += 100 * value.item()
+        print("{}th music finish".format(i+1))
+    
+    for k, v in result.items():
+        result_dict[k] = round(v/n, 2)
+    
+with open("python2json.json", 'w') as f:
+  json.dump(result_json, f)
