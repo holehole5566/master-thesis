f7a191e582fdbb0743d379f1ca6821db669be73d
holehole5566 <jd965303@gmail.com>
2023-11-20 23:58:56 +0800

    update

--- classification.py
+++ classification.py
@@ -1,53 +1,53 @@
 import torch.nn.functional as F
 import json
 from msclap import CLAP
-from collections import defaultdict 
+from collections import defaultdict
 
-feature_classes = [['fast rhythm','slow rhythm'],['high pitch','low pitch'],['loud intensity','soft intensity'],['bright timbre','mellow timbre']]
-eval_classes = [['high arousal','low arousal'],['positive valence','negative valence']]
+def get_audio_files(n):
+    audio_files = []
+    if n > 250:
+        for i in range(0, n, 3):
+            music = f'music/music ({i+1}).mp3'
+            audio_files.append(music)
+    else:
+        for i in range(n):
+            music = f'music/music ({i+1}).mp3'
+            audio_files.append(music)
+    return audio_files
 
-result_dict = defaultdict(float)
-prompt = 'music with '
+def process_audio_files(audio_files, class_prompts):
+    clap_model = CLAP(version='2023', use_cuda=True)
+    text_embeddings = clap_model.get_text_embeddings(class_prompts)
+    audio_embeddings = clap_model.get_audio_embeddings(audio_files, resample=True)
+    return clap_model.compute_similarity(audio_embeddings, text_embeddings)
 
-n = 251
+def main():
+    feature_classes = [['fast rhythm', 'slow rhythm'], ['high pitch', 'low pitch'], ['loud intensity', 'soft intensity'], ['bright timbre', 'mellow timbre']]
+    eval_classes = [['high arousal', 'low arousal'], ['positive valence', 'negative valence']]
+    result_dict = defaultdict(float)
+    prompt = 'music with '
 
-for classes in feature_classes:
-    
-    class_prompts = []
-    
-    for desc in classes:
-        class_prompts.append(prompt + desc)
-    
-    print(class_prompts)
-    
-    for i in range(0, n, 3):
+    num_music = 662
+    audio_files = get_audio_files(num_music)
 
-        audio_files = []
-        music = 'music/music (' + str(i+1) + ').mp3'
-        audio_files.append(music)
-        # Load and initialize CLAP
-        # Setting use_cuda = True will load the model on a GPU using CUDA
-        clap_model = CLAP(version = '2023', use_cuda=True)
+    for classes in feature_classes:
+        class_prompts = [prompt + desc for desc in classes]
+        print(class_prompts)
+        for i in range(len(audio_files)):
+            audio_file = []
+            audio_file.append(audio_files[i])
+            similarity = process_audio_files(audio_file, class_prompts)
+            similarity = F.softmax(similarity, dim=1)
+            values, indices = similarity[0].topk(2)
+            for value, index in zip(values, indices):
+                result_dict[classes[index]] += 100 * value.item()
+            print(f"{i+1}th music finish")
 
-        # compute text embeddings from natural text
-        text_embeddings = clap_model.get_text_embeddings(class_prompts)
+    for k, v in result_dict.items():
+        result_dict[k] = round(v / num_music, 2)
 
-        # compute the audio embeddings from an audio file
-        audio_embeddings = clap_model.get_audio_embeddings(audio_files, resample=True)
+    with open("result.json", 'w') as f:
+        json.dump(result_dict, f)
 
-        # compute the similarity between audio_embeddings and text_embeddings
-        similarity = clap_model.compute_similarity(audio_embeddings, text_embeddings)
-
-        similarity = F.softmax(similarity, dim=1)
-        values, indices = similarity[0].topk(2)
-
-        for value, index in zip(values, indices):
-             #print(f"{classes[index]:>16s}: {100 * value.item():.2f}%")
-            result_dict[classes[index]] += 100 * value.item()
-        print("{}th music finish".format(i+1))
-    
-for k, v in result_dict.items():
-        result_dict[k] = round(v/n, 2)
-    
-with open("result.json", 'w') as f:
-  json.dump(result_dict, f)
+if __name__ == "__main__":
+    main()
